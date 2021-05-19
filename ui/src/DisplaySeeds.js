9e45a9bbdb6b4ad6a665bfe68d69bad75c58f28e
Blaine Hill <blainehill2001@gmail.com>
2021-05-19 16:04:34 -0400

    Fixed image stretch bug

--- ui/src/DisplaySeeds.js
+++ ui/src/DisplaySeeds.js
@@ -12,8 +12,8 @@ const DisplaySeeds = ({ seedTracks, deleteSeed }) => {
                             return(
                                 <div class="container mx-auto w-1/2 lg:w-1/5 md:w-1/3 p-3 h-full" key={seedTrack.id} id="heightgrabber">
                                     <button name={seedTrack.name} class="w-full h-full p-4 ring ring-transparent rounded-lg bg-green-800 hover:bg-blue-500 active:bg-indigo-800" onClick={() => deleteSeed(seedTracks, seedTrack)}>
+                                        <img src={seedTrack.album.images[0].url} class="object-scale-down"/>
                                         <div class="flex flex-row flex-wrap justify-around">
-                                            <img src={seedTrack.album.images[0].url} class="object-scale-down"/>
                                             <p class="p-1 text-xs sm:text-sm md:text-base lg:text-lg">{seedTrack.name} <br /> <i>by {seedTrack.artists[0].name}</i></p>
                                         </div>
                                     </button>
