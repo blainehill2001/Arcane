a8ad760d2bc2bfefa1d80de0e0c66be5b4cd9f8a
Blaine Hill <blainehill2001@gmail.com>
2023-01-12 10:29:19 -0600

    used node-prune

--- ui/craco.config.js
+++ ui/craco.config.js
@@ -1,10 +1,13 @@
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
-    style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
-  }
+  },
+  webpack: {
+    plugins: [new BundleAnalyzerPlugin({ analyzerMode: "server" })],
+  },
+};
